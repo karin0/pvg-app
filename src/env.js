31e404e8e8185213bec462e206bb3f4303b3de27
karin0 <karin0@gmx.com>
2024-05-11 18:47:38 +0800

    env: detect development API host from environment

--- src/env.js
+++ src/env.js
@@ -1,10 +1,16 @@
-const port =
-  process.env['NODE_ENV'] === 'development' ? 5678 : window.location.port
-const host =
-  window.location.protocol +
-  '//' +
-  window.location.hostname +
-  (port ? ':' + port + '/' : '/')
+let host = localStorage.getItem('dev_host')
+if (!host) {
+  host = process.env['REACT_APP_API_HOST']
+  if (!host) {
+    const port =
+      process.env['NODE_ENV'] === 'development' ? 5678 : window.location.port
+    host =
+      window.location.protocol +
+      '//' +
+      window.location.hostname +
+      (port ? ':' + port + '/' : '/')
+  }
+}
 
 const images_per_page = 50
 const upscale_target = Math.max(
